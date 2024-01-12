4447955fc45b71ab9a9fe019bdbbae09b4e14cde
carlos eduardo <127193594+Carloseduardomelo@users.noreply.github.com>
2024-01-12 16:06:21 -0300

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,46 +2,50 @@ import style from "./Footer.module.css";
 import casa from "../../assets/logo.svg";
 import face from "../../assets/f.svg";
 import t from "../../assets/T.png"
+import linktin from "../../assets/link.svg"
+import instagram from "../../assets/insta.svg"
 
 const Footer = () => {
   return (
-    <div className={style}>
+    <div className={style.divAll}>
       <section>
-        <section>
+        <section className={style.selhono}>
           <img src={casa} alt="logo da casa." />
           <p>Selhono</p>
         </section>
-        <p className={style.my_footer}>
+        <p className={style.established}>
           It is a long established fact that a reader will be distracted
           lookings.
         </p>
         <section>
           <img src={face} alt="" />
           <img src={t} alt="" />
+          <img src={linktin} alt="" />
+          <img src={instagram} alt="" />
         </section>
       </section>
 
       <section>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
+        <p>Pages</p>
+        <p>About Us</p>
+        <p>Our Projects</p>
+        <p>Our Team</p>
+        <p>Contac Us</p>
+        <p>Services</p>
       </section>
       <section>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
+        <p>Services</p>
+        <p>Kitchan</p>
+        <p>Living Area</p>
+        <p>Bathrcom</p>
+        <p>Dinning Hall</p>
+        <p>Bedroom</p>
       </section>
       <section>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
+        <p>Contact</p>
+        <p>55 East Birchwood Ave. Brooklyn, New York 11201</p>
+        <p>contact@selhono.com</p>
+        <p>(123) 456 - 7890</p>
       </section>
     </div>
   );
