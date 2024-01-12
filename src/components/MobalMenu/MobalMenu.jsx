72daeaf6f500b508c8e3eab0eb4030a691f74afe
Manoel <999142818m@gmail.com>
2024-01-12 16:58:19 -0300

    manu feito, menu mobile em andamento

---
+++ src/components/MobalMenu/MobalMenu.jsx
@@ -0,0 +1,20 @@
+import Search from "../../assets/search.svg";
+import style from "./MobalMenu.module.css";
+
+const MobalMenu = () => {
+    return (
+        <nav className={style.Nav}>
+            <ul className={style.Navlinks}>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Pages</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Project</a></li>
+            <li><a href="#">Blog</a></li>
+            <li><a href="#">Contact</a></li>
+            <img src={Search} />
+            </ul>
+        </nav>
+    );
+};
+
+export default MobalMenu;
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,11 +1,33 @@
+import Logo from "../../assets/logo.svg";
+import Search from "../../assets/search.svg";
+
+import MobalMenu from "../MobalMenu/MobalMenu";
+
+import style from "./header.module.css";
+
 const Header = () => {
-    return (
-        <div>
-            <div>
-                <h1>Header</h1>
-            </div>            
+  return (
+      <div className={style.Header}>
+        <div className={style.Page}>
+        <div className={style.Logo}>
+          <img src={Logo} className={style.Image} />
+          <h1>SELHONO</h1>
         </div>
-    );
+        <nav className={style.Nav}>
+          <ul className={style.Navlinks}>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Pages</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Project</a></li>
+            <li><a href="#">Blog</a></li>
+            <li><a href="#">Contact</a></li>
+            <img src={Search} />
+          </ul>
+        </nav>
+        <MobalMenu />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
