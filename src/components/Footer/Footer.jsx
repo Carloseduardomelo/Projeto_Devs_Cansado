248fda4e58182a4e4c6edcc7a3aac0a51b67f3c9
carlos eduardo <127193594+Carloseduardomelo@users.noreply.github.com>
2024-01-12 15:21:09 -0300

    img footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,45 @@
+import style from "./Footer.module.css"
+
+
+
+const Footer = () => {
+  return (
+    <div className={}>
+        <section>
+            <section>
+                <img src="/material-symbols-light_other-houses.svg" alt="logo da casa." />
+                <p>Selhono</p>
+            </section>
+            <p>It is a long established fact that a reader will be distracted lookings.</p>
+            <section>
+
+            </section>
+        </section>
+
+        <section>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+        </section>
+        <section>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+        </section>
+        <section>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+        </section>
+    </div>
+  )
+}
+
+export default Footer
