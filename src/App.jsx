6d984984102edb1df65ec4b258e25f6a02dbfaf2
Manoel <999142818m@gmail.com>
2024-01-12 21:32:50 -0300

    Header completo, responsividade Ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import Main from './components/main/main'
 import './App.css'
-import Footer from './components/Footer/Footer'
+// import Footer from './components/Footer/Footer'
+import Header from './components/header/header'
 
 function App() {
 
   return (
     <div className='app'>
+      <Header />
       <Main />
-      <Footer />
+      {/* <Footer /> */}
     </div>
   )
 }
--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,10 +1,7 @@
-import Header from "../header/header";
-
 const Main = () => {
     return (
         <div>
-            <Header />
-            <h1>Main</h1>
+            
         </div>
     );
 };
