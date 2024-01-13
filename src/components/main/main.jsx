18ac1c13a96d8fcb7ad39fdb70fa0073b2f57320
carlos eduardo <127193594+Carloseduardomelo@users.noreply.github.com>
2024-01-12 22:03:19 -0300

    final

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,37 +1,18 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-const Main = () => {
-    return (
-        <div>
-            
-=======
-=======
-import InfesProjetes from '../InfesProjetes/InfesProjetes';
->>>>>>> 482b142 (segunda parte)
->>>>>>> baf91ee49c5fe4f5a68c86841e72a412cdd8f53b
-import style from './main.module.css'
+import InfesProjetes from "../InfesProjetes/InfesProjetes";
+import style from "./main.module.css"
 
 const Main = () => {
     return (
         <div className={style.Page}>
-            <section className={style.Page_title}>
+            {/* <section className={style.Page_title}>
                 <h1>Lets make your home beautiful together</h1>
                 <p>There are many variations of the passages of lorem Ipsum fromavailable, majority.</p>
                 <button className={style.Button}>
                     <a href="#">Get Started</a>
                 </button>
-            </section>
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> b826c2021a784466b96da1f86c7c232c2a18c024
-=======
-
+            </section> */}
             <InfesProjetes />
->>>>>>> 482b142 (segunda parte)
->>>>>>> baf91ee49c5fe4f5a68c86841e72a412cdd8f53b
+
         </div>
     );
 };
