2ad6eec1957b7194708c901cd9162bca43451f2f
devjoaocarlos <devjoaocarlos@outlook.com>
2024-01-12 21:13:54 -0300

    update main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import Main from './components/main/main'
 import './App.css'
-import Footer from './components/Footer/Footer'
+import Header from './components/header/header'
 
 function App() {
 
   return (
     <div className='app'>
+      <Header />
       <Main />
-      <Footer />
     </div>
   )
 }
--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,10 +1,15 @@
-import Header from "../header/header";
+import style from './main.module.css'
 
 const Main = () => {
     return (
-        <div>
-            <Header />
-            <h1>Main</h1>
+        <div className={style.Page}>
+            <section className={style.Page_title}>
+                <h1>Lets make your home beautiful together</h1>
+                <p>There are many variations of the passages of lorem Ipsum fromavailable, majority.</p>
+                <button className={style.Button}>
+                    <a href="#">Get Started</a>
+                </button>
+            </section>
         </div>
     );
 };
