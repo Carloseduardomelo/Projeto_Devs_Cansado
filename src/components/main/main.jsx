b2a2f5b6d96ca66b28496f8d303db5bb8c0018cc
devjoaocarlos <devjoaocarlos@outlook.com>
2024-01-12 22:08:14 -0300

    update

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,17 +1,21 @@
-import style from './main.module.css'
+import style from "./main.module.css";
+import FundoSala from "../../assets/fundosala.svg";
+import Seta from "../../assets/Vector.svg";
 
 const Main = () => {
-    return (
-        <div className={style.Page}>
-            <section className={style.Page_title}>
-                <h1>Lets make your home beautiful together</h1>
-                <p>There are many variations of the passages of lorem Ipsum fromavailable, majority.</p>
-                <button className={style.Button}>
-                    <a href="#">Get Started</a>
-                </button>
-            </section>
-        </div>
-    );
+  return (
+    <div className={style.Page}>
+      <img src={FundoSala} alt="" />
+      <section className={style.Page_title}>
+        <h1>Lets make your home beautiful together</h1>
+        <p>There are many variations of the passages of lorem Ipsum fromavailable, majority.</p>
+        <button className={style.Button}>
+          <img src={Seta} alt="" />
+          <a href="#">Get Started</a>
+        </button>
+      </section>
+    </div>
+  );
 };
 
 export default Main;
