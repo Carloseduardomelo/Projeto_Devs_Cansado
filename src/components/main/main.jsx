8a36deb78b8706312c6712eea5095536a0cfc960
Manoel <999142818m@gmail.com>
2024-01-12 22:05:16 -0300

    up

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,16 +1,4 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-const Main = () => {
-    return (
-        <div>
-            
-=======
-=======
 import InfesProjetes from '../InfesProjetes/InfesProjetes';
->>>>>>> 482b142 (segunda parte)
->>>>>>> baf91ee49c5fe4f5a68c86841e72a412cdd8f53b
 import style from './main.module.css'
 
 const Main = () => {
@@ -23,15 +11,7 @@ const Main = () => {
                     <a href="#">Get Started</a>
                 </button>
             </section>
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> b826c2021a784466b96da1f86c7c232c2a18c024
-=======
-
             <InfesProjetes />
->>>>>>> 482b142 (segunda parte)
->>>>>>> baf91ee49c5fe4f5a68c86841e72a412cdd8f53b
         </div>
     );
 };
